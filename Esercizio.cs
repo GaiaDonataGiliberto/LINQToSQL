1ee4a5a5ac31677843bfb17e2e88df76e46787cd
Gaia Donata Giliberto <gaia.giliberto@gmail.com>
2020-11-26 18:26:11 +0100

    Update

--- Esercizio.cs
+++ Esercizio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -70,6 +71,93 @@ namespace LINQToSQL
             }
 
             SelectMovies();
+
+        }
+
+
+        public static void DeleteMovie(int id)
+        {
+            CinemaDBDataContext db = new CinemaDBDataContext(conString);
+
+            var movieToDelete = db.movies.SingleOrDefault(m => m.ID == id);
+
+            if (movieToDelete != null)
+            {
+                db.movies.DeleteOnSubmit(movieToDelete);
+                Console.WriteLine("Film cancellato");
+            }
+
+            db.SubmitChanges();
+
+        }
+
+        public static void UpdateMovieByTitolo()
+        {
+            CinemaDBDataContext db = new CinemaDBDataContext(conString);
+
+            Console.WriteLine("Dimmi il titolo del film da aggiornare: ");
+            string titolo = Console.ReadLine();
+
+            IQueryable<movy> filmByTitolo =
+                from film in db.movies
+                where film.titolo == titolo
+                select film;
+
+            Console.WriteLine("I film trovati sono: {0}", filmByTitolo.Count());
+
+            if (filmByTitolo.Count() == 0)
+            {
+                return;
+            }
+
+            if (filmByTitolo.Count() > 1)
+            {
+                return;
+            }
+
+
+            SelectMovies();
+
+            Console.WriteLine("Scrivere is valori aggiornati ");
+            Console.WriteLine("Titolo: ");
+            string titolo1 = Console.ReadLine();
+
+            Console.WriteLine("Genere: ");
+            string genere = Console.ReadLine();
+
+            Console.WriteLine("Durata: ");
+            int durata = Convert.ToInt32(Console.ReadLine());
+
+
+            foreach (var f in filmByTitolo)
+            {
+                f.titolo = titolo1;
+                f.genere = genere;
+                f.durata = durata;
+            };
+
+            try
+            {
+                Console.WriteLine("Premi un tasto per mandare modifiche al db");
+                Console.ReadKey();
+                db.SubmitChanges(ConflictMode.FailOnFirstConflict);
+
+            }
+            catch (ChangeConflictException e)
+            {
+
+                Console.WriteLine("Concurrency error");
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+
+                //OverwriteCurrentValues->ignora le mie modifiche(aggiorna il mio obj model)
+                //KeepCurrentValues->ignora le modifiche altrui(sovrascrive il db con il mio obj model)
+                //KeepChanges->cerca di tenere entrambe le modifiche 
+                db.ChangeConflicts.ResolveAll(RefreshMode.OverwriteCurrentValues);
+
+                db.SubmitChanges();
+            }
+
 
         }
     }
